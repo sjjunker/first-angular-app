a3c1c23948e61de3cd112f76c3c08cf20b957cff
Sandi Junker <sandi.junker@outlook.com>
2025-04-24 09:30:18 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
